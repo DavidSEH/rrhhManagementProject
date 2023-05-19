28bfd901261959af11d17145842d25cbe308fc4b
DavidEH <dseh.e.h@gmail.com>
2023-05-19 00:19:30 -0500

    Actualizando el index

--- index.php
+++ index.php
@@ -1,73 +1,161 @@
-<!DOCTYPE html>
-<html lang="es">
+<!doctype html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Diseño Hotel</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/estiloindex.css">
+    <title>Talenty</title>
 </head>
 <body>
-    <header class="main-header">
-        <div class="container container--flex">
-            <h1 class="main-title"><span class="color-span">ControlGestion</span>RRHH</h1>
-            <nav class="main-nav">
-                <span class="icon-menu" id="btn-menu"><i class="fas fa-bars"></i></span>
-                <div id="categorias">
-                    <ul class="menu" id="menu">
-                        
-                        <li class="menu__item"><a href="" class="menu__link"><span> </span></a></li>
-                        <li class="menu__item"><a href="" class="menu__link"><span> </span></a></li>
-                        <li class="menu__item"><a href="" class="menu__link menu__link--select"><span>Inicio</span></a></li>
-                        
-                        <li class="menu__item">
-                            <a class="menu__link"><span>Login</span></a>
-                            <ul class="menu_desplegable">
-                                <li><a href="MVC/Vista/loginCliente.php" ><span>Empleado</span></a></li></li>
-                                <li><a href="MVC/Vista/loginUsuario.php" ><span>Admin</span></a></li></li>
-                            </ul>
-                        </li>
-                        
+<header>
+    <div class="container__header">
+        <div class="logo">
+            <img src="imagenes/talenty_rbg.png" alt="logo de la empresa">
+        </div>
+        <nav>
+            <ul class="list">
+                <li><a href="#soluciones">Soluciones</a></li>
+                <li><a href="#contactanos">Contactanos</a></li>
+                <li class="session">Inicia Sesion
+                    <ul class="sesion_list">
+                        <li><a href="MVC/Vista/loginCliente.php">Empleado</a></li>
+                        <li><a href="MVC/Vista/loginUsuario.php">Administrador</a></li>
                     </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<section class="first-section">
+    <div class="fs-fst__element">
+        <h1>¿Que es Talenty?</h1>
+        <p>Es un sistema web de gestión de Recursos Humanos que te permite agilizar procesos administrativos de tu
+            empresa: Cálculo de pago de planilla, Gestión de Personas, Gestión de Talentos y mas</p>
+        <div class="section__img1">
+            <img src="imagenes/body.png" alt="">
+        </div>
+    </div>
+    <div class="fs-snd__element">
+        <div class="section__img2">
+            <img src="imagenes/body.png" alt="">
+        </div>
+        <div class="section__img3">
+            <img src="imagenes/body.png" alt="">
+        </div>
+    </div>
+</section>
+<section class="second-section">
+    <div class="sd-fst__element">
+        <div class="sd_block">
+            <h2 id="soluciones">Soluciones</h2>
+            <div class="solutions">
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Reclutamiento y Selección</h4>
+                    <p>Ten la oportunidad de elegir al mejor talento para tu equipo, nuestro software te ayuda a llevar
+                        este proceso de manera rápida.</p>
                 </div>
-            </nav> 
-        </div>  
-    </header>
-    <div class="banner">
-        <div class="banner__content">
-            <div class="container">
-                <h2 class="banner__tittle">Talenti S.A.</h2>
-                <p class="banner__txt">SISTEMA PARA LA GESTION DE RECURSOS HUMANOS </p>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Onboarding</h4>
+                    <p>Tus colaboradores podrán tener tareas personalizadas e individuales para ellos durante su proceso
+                        de incorporación a la empresa.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Gestión de Personas</h4>
+                    <p>Ten la información de tus colaboradores a tu alcance de manera personalizada, genera reportes e
+                        integra tu información con el sistema.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Asistencia</h4>
+                    <p>Gestiona la entrada y salida de tus colaboradores, vacaciones y horas extras de tu equipo, desde
+                        una misma plataforma.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Sistema de Planillas</h4>
+                    <p>Automatiza tus tareas de cálculo de planillas y olvídate de los errores manuales, te ayudamos con
+                        la actualización de los parámetros de SUNAT.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Firma Electrónica</h4>
+                    <p>Reduce costos con la firma electrónica, acelera tus gestiones administrativas con una mayor
+                        seguridad.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Desempeño</h4>
+                    <p>Empieza a medir tus objetivos evaluando la productividad de sus colaboradores a través de un
+                        feedback continuo.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Capacitaciones</h4>
+                    <p>Con nuestra plataforma LMS podrás brindarle a tus colaboradores cursos personalizados para
+                        capacitarlos, y así potenciar sus conocimientos.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Vacaciones</h4>
+                    <p>Gestiona los días de vacaciones acumulados por periodos y obten reportes de días acumulados
+                        próximos al límite, agrupados por periodos y mucho más.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Comunicación y Reconocimiento</h4>
+                    <p>Mantén una comunicación constante con tus colaboradores y conoce más sobre ellos a través de
+                        nuestra plataforma, para así reconocerlos por su mérito.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Encuestas</h4>
+                    <p>A través de nuestra plataforma podrás crear encuestas únicas y personalizadas, teniendo los
+                        resultados de manera automática.</p>
+                </div>
+                <div class="element_solution">
+                    <img src="imagenes/icons8-chevron-right-48.png" alt="">
+                    <h4>Beneficios</h4>
+                    <p>Impulsa el rendimiento de tu equipo brindándoles beneficios que vayan de la mano con sus
+                        necesidades.</p>
+                </div>
             </div>
         </div>
     </div>
-    <main class="main" copy>
-        
-        <section class="newsletter">
-            
-            
-        </section>    
-        
-    </main>
-    <footer class="main-footer">
-        <div class="container ">
-        <div class="column column--50-25">
-            <h2 class="footer__title">La empresa</h2>
-            <p class="footer__txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Impedit neque dolore inventore exercitationem officiis obcaecati minima aspernatur, 
-                cum ut maiores ullam voluptas ipsa iste alias rem? Tenetur sint vel assumenda.</p>
-            <cite class="footer__author"></cite> 
-        </div>
-        
-        
-        
-        </div>
-        <p class="copy">  <span class="color-span"></span> </p>
+    <div class=sd-snd__element">
+        <div class="snd__img">
 
-    </footer>
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="sd_block2">
+        <h2 id="contactanos">Contactanos</h2>
+        <div class="contactanos">
+            <div class="telefono bloque">
+                <h4>Telefono</h4>
+                <p>(01) 289-2240</p>
+            </div>
+            <div class="correo bloque">
+                <h4>Correo Electrónico</h4>
+                <p>consultas@talenty.com</p>
+            </div>
+            <div class="direccion bloque">
+                <h4>Dirección</h4>
+                <p>Jiron Quilca 219, 15001</p>
+                <p>Centro de Lima, Lima - Perú</p>
+            </div>
+            <div class="horario bloque">
+                <h4>Horario</h4>
+                <p>Lunes a Viernes de 9 AM a 18 PM</p>
+            </div>
 
-    <script src="js/menu.js"></script>
+        </div>
+    </div>
+</footer>
 </body>
-
 </html>
