f11f7317aaf120725c21df2f4ff9ad5f76a003b9
DavidEH <dseh.e.h@gmail.com>
2023-07-20 00:40:34 -0500

    .

--- MVC/Modelo/conexion.php
+++ MVC/Modelo/conexion.php
@@ -1,14 +1,4 @@
 <?php
 
-        $host = 'localhost:3306';
-        $user = 'root';
-        $password = '';
-        $db = 'tutrabajo';
-
-        $conection = @mysqli_connect($host,$user,$password,$db);
-
-        if(!$conection){
-            echo "Error en la conexión";
-        }
 
 ?>
